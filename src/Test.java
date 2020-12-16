399556f7411d9e7c6d351de5ab295c3b99fdad2b
cccgl <1243610440@qq.com>
2020-12-16 22:09:47 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+public class Test {
+
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address1=InetAddress.getByName("127.0.0.1");
+        System.out.println(address1);
+        InetAddress address2=InetAddress.getByName("www.bilibili.com");
+        System.out.println(address2);
+        InetAddress address3=InetAddress.getLocalHost();
+        System.out.println(address3);
+
+        System.out.println(InetAddress.getLocalHost());
+    }
+
+}
---
+++ src/com/lei/ClientDemo01.java
@@ -0,0 +1,40 @@
+package com.lei;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientDemo01 {
+    public static void main(String[] args) throws IOException {
+       // InetAddress serverIP=InetAddress.getByName("127.0.0.1");
+        InetAddress serverIP=null;
+        int port;
+        Socket socket=null;
+        OutputStream os=null;
+        try {
+            //本机ip
+            serverIP=InetAddress.getByName("127.0.0.1");
+            //设置交互端口号
+            port=9999;
+            //设置套接字
+            socket=new Socket(serverIP,port);
+            //输出流
+            os=socket.getOutputStream();
+            //写下hello world
+            os.write("hello world".getBytes());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            //关闭资源 从下到上
+            os.close();
+            socket.close();
+
+
+        }
+    }
+}
